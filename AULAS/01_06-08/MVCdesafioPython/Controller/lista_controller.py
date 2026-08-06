3e6257e29a635d63b4870eab6ed2279bbab92f72
gabrielbraganholo <gabrielmbragaholo@gmail.com>
2026-08-06 11:04:03 -0300

    começando desafio mvc python

---
+++ AULAS/01_06-08/MVCdesafioPython/Controller/lista_controller.py
@@ -0,0 +1,2 @@
+class Lista_Controller:
+    
---
+++ AULAS/01_06-08/MVCdesafioPython/Models/utilidades.py
@@ -0,0 +1,10 @@
+class Utilidades:
+    """
+    Classe responsável por fornecer métodos auxiliares para manipulação de listas
+    """
+
+    def popular_lista(lista, quantidade_numeros, inicio, fim, aleatorio){
+        """
+        Popula uma lsta com 
+        """
+    }
